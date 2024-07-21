b25312169517fc4e96bc2218dbd92ae85c8a7262
CondoriWilliam <wcondorian@unsa.edu.pe>
2024-07-21 12:22:34 -0500

    Creando nueva rama

---
+++ Assets/Scrpts/MoverCamara.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCamara : MonoBehaviour
+{
+    public GameObject Target;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, transform.position.z);
+    }
+}
